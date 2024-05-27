bc17acc2c07dc10e34caa375f8daa51bb542d46d
kennethtkatsande <kennytakudzwa@gmail.com>
2024-05-27 04:38:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Typescript Tutorial
+
+1. Introduction
+2. Install Tools
+3. Create Typescript React App by Vite
+   Create Git repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <header>TS Arizona</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  )
+}
+
+export default App
