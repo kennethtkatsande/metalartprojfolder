628ad27225e386c8deaee03049430bb07b5c5771
kennethtkatsande <kennytakudzwa@gmail.com>
2024-05-29 14:21:38 +0200

    routing from home page to product page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
-import { Container, Navbar, Nav, Row, Col } from 'react-bootstrap'
-import { sampleProducts } from './data'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+{
+  /* import { sampleProducts } from './data' */
+}
 
 function App() {
   return (
@@ -21,19 +24,7 @@ function App() {
       </header>
       <main>
         <Container className="mt-3">
-          <Row>
-            {sampleProducts.map((product) => (
-              <Col key={product.slug} sn={6} md={4} lg={3}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="product-image"
-                />
-                <h2>{product.name}</h2>
-                <p>${product.price}</p>
-              </Col>
-            ))}
-          </Row>
+          <Outlet></Outlet>
         </Container>
       </main>
       <footer>
