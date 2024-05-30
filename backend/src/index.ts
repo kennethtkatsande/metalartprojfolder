8328a1717ec1d60d3f0da795d99e2ed987214e08
kennethtkatsande <kennytakudzwa@gmail.com>
2024-05-30 20:14:54 +0200

    up to end of lesson 9

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
+import cors from 'cors'
 import express, { Request, Response } from 'express'
 import { sampleProducts } from './data'
 
 const app = express()
-app.get('api/products', (req: Request, res: Response) => {
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173'],
+  })
+)
+
+app.get('/api/products', (req: Request, res: Response) => {
   res.json(sampleProducts)
 })
 const PORT = 4000
