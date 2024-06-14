20b28b9a3fe93b9c6f2123c46d8ab4a8b926b34f
kennethtkatsande <kennytakudzwa@gmail.com>
2024-06-14 10:30:56 +0200

    implementing add to cart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from 'react'
-import { Container, Navbar, Nav, Button } from 'react-bootstrap'
-import { Outlet } from 'react-router-dom'
+import { Container, Navbar, Nav, Button, Badge } from 'react-bootstrap'
+import { Link, Outlet } from 'react-router-dom'
 import { Store } from './Store'
 {
   /* import { sampleProducts } from './data' */
@@ -8,7 +8,7 @@ import { Store } from './Store'
 
 function App() {
   const {
-    state: { mode },
+    state: { mode, cart },
     dispatch,
   } = useContext(Store)
 
@@ -31,9 +31,14 @@ function App() {
             <Button variant={mode} onClick={switchModeHandler}>
               <i className={mode === 'light' ? 'fa fa-sun' : 'fa fa-moon'}></i>
             </Button>
-            <a href="/cart" className="nav-link">
+            <Link to="/cart" className="nav-link">
               Cart
-            </a>
+              {cart.cartItems.length > 0 && (
+                <Badge pill bg="danger">
+                  {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                </Badge>
+              )}
+            </Link>
             <a href="/signin" className="nav-link">
               Sign In
             </a>
--- frontend/src/components/ProductItem.tsx
+++ frontend/src/components/ProductItem.tsx
@@ -2,8 +2,31 @@ import { Product } from '../types/Product'
 import { Link } from 'react-router-dom'
 import { Button, Card } from 'react-bootstrap'
 import Rating from './Rating'
+import { useContext } from 'react'
+import { Store } from '../Store'
+import { CartItem } from '../types/Cart'
+import { convertProductToCartItem } from '../utils'
 
 function ProductItem({ product }: { product: Product }) {
+  const { state, dispatch: ctxDispatch } = useContext(Store)
+  const {
+    cart: { cartItems },
+  } = state
+
+  const addToCartHandler = async (item: CartItem) => {
+    const existItem = cartItems.find((x) => x._id === product._id)
+    const quantity = existItem ? existItem.quantity + 1 : 1
+    if (product.countInStock < quantity) {
+      alert('Sorry. Product is out of stock')
+      return
+    }
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    })
+    alert('Product added to the cart')
+  }
+
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
@@ -20,7 +43,11 @@ function ProductItem({ product }: { product: Product }) {
             Out of Stock
           </Button>
         ) : (
-          <Button>Add to cart</Button>
+          <Button
+            onClick={() => addToCartHandler(convertProductToCartItem(product))}
+          >
+            Add to cart
+          </Button>
         )}
       </Card.Body>
     </Card>
