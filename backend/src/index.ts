f0c73faf75789471f0d0c4ad2f710b77d0534c19
kennethtkatsande <kennytakudzwa@gmail.com>
2024-06-27 14:58:40 +0200

    building sign in backend api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import { productRouter } from './routers/productRouter'
+import { seedRouter } from './routers/seedRouter'
+import { userRouter } from './routers/userRouter'
 
 dotenv.config()
 const MONGODB_URI =
@@ -25,7 +27,12 @@ app.use(
   })
 )
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use('/api/products', productRouter)
+app.use('api/users', userRouter)
+app.use('/api/seed', seedRouter)
 
 const PORT = 4000
 app.listen(PORT, () => {
