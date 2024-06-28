3d14a5df8d0d561eeb543dabc48df7eb596072e7
kennethtkatsande <kennytakudzwa@gmail.com>
2024-06-28 10:33:39 +0200

    creating the details of the  order page and functionality

--- backend/src/routers/orderRouter.ts
+++ backend/src/routers/orderRouter.ts
@@ -5,6 +5,20 @@ import { OrderModel } from '../models/orderModel'
 import { Product } from '../models/productModel'
 export const orderRouter = express.Router()
 
+orderRouter.get(
+  // /api/orders/:id
+  '/:id',
+  isAuth,
+  asyncHandler(async (req: Request, res: Response) => {
+    const order = await OrderModel.findById(req.params.id)
+    if (order) {
+      res.json(order)
+    } else {
+      res.status(404).send({ message: 'Order Not Found' })
+    }
+  })
+)
+
 orderRouter.post(
   '/',
   isAuth,
@@ -29,3 +43,14 @@ orderRouter.post(
     }
   })
 )
+function asyncHandler(
+  arg0: (req: Request, res: Response) => Promise<void>
+): import('express-serve-static-core').RequestHandler<
+  import('express-serve-static-core').ParamsDictionary,
+  any,
+  any,
+  import('qs').ParsedQs,
+  Record<string, any>
+> {
+  throw new Error('Function not implemented.')
+}
