7c6751617cdc75fa2acc46a8f092495fa312ea2d
kennethtkatsande <kennytakudzwa@gmail.com>
2024-06-28 12:13:19 +0200

    adding paypal payment

--- backend/src/routers/orderRouter.ts
+++ backend/src/routers/orderRouter.ts
@@ -43,6 +43,31 @@ orderRouter.post(
     }
   })
 )
+
+orderRouter.put(
+  '/:id/pay',
+  isAuth,
+  asyncHandler(async (req: Request, res: Response) => {
+    const order = await OrderModel.findById(req.params.id).populate('user')
+
+    if (order) {
+      order.isPaid = true
+      order.paidAt = new Date(Date.now())
+      order.paymentResult = {
+        paymentId: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      }
+      const updatedOrder = await order.save()
+
+      res.send({ order: updatedOrder, message: 'Order Paid Successfully' })
+    } else {
+      res.status(404).send({ message: 'Order Not Found' })
+    }
+  })
+)
+
 function asyncHandler(
   arg0: (req: Request, res: Response) => Promise<void>
 ): import('express-serve-static-core').RequestHandler<
